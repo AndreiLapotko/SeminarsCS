2bfc44c53bf0f72dd6eac64ba90d4b9304a763ee
AndreiLapotko <shudyx@gmail.com>
2023-09-19 02:14:59 +1000

    corrected Task02

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -2,9 +2,9 @@
 // принимает два числа и выдаёт, какое число большее, а
 // какое меньшее.
 
-Console.Write("Введите первое число a: ");
+Console.Write("Введите первое любое целое число a: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число b: ");
+Console.Write("Введите второе любое целое число b: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
 if (firstNumber > secondNumber) Console.WriteLine($"a = {firstNumber}; b = {secondNumber} -> max = {firstNumber}");
