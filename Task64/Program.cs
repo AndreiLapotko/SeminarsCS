920d36c04cab3c94d77aab7a5fb3cb110c12c2b8
AndreiLapotko <shudyx@gmail.com>
2023-10-16 20:14:53 +1000

    added Tasks from 63 to 69

---
+++ Task64/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int GetUserInputInt(string msg)
+{
+    int num = 0;
+    bool check = CheckInput(num); 
+    while (!check)
+    {
+        Console.Write($"{msg}: ");
+        string? value = Console.ReadLine();
+        num = Convert.ToInt32(value);
+        check = CheckInput(num);
+        if (!check) Console.WriteLine("Введите корректные данные!");
+    }
+    return num;
+}
+
+bool CheckInput(int n)
+{
+    return (n < 1 ? false : true);
+}
+
+void CountDown(int n)
+{
+    if (n == 0) return;
+    Console.Write(n == 1? $"{n}\"" : $"{n}, ");
+    CountDown(n - 1);
+    
+}
+
+int n = GetUserInputInt("Введите натуральное число");
+Console.Write($"N = {n} -> \"");
+CountDown(n);
---
+++ Task69/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+int UserNumber(string message)
+{
+    Console.WriteLine(message);
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+
+double PoweringAbyB(int a, int b) // 2,-2  2,-1  2,0
+{
+    // if (b > 0)
+    // {
+    //     if (b == 0) return 1; //3,0
+    //     return a * PoweringAbyB(a, b - 1); //3,5 - 3,4 - 3,3 - 3,2 - 3,1
+    //     //return 1 * 3 * 3 * 3 * 3 * 3 = 243
+    // }
+    // else
+    // {
+    //     double result = 1;
+    //     if (b == 0) return 1;
+    //     result = 1/(a * PoweringAbyB(a, b + 1)); // не работает!!!!
+    //     return result; // 2,-2  2,-1  
+    //     //    1 * 2 * 2 = 4  
+    // }
+    if (b == 0) return 1;
+    if (b < 0) return (1 / PoweringAbyB(a, -b));
+    return a * PoweringAbyB(a, b - 1);
+}
+
+
+int a = UserNumber("Enter digit a: ");
+int b = UserNumber("Enter power b: ");
+double powerAB = 0;
+// if (b >= 0)
+// powerAB = PoweringAbyB(a, b);
+// else 
+// powerAB = 1/PoweringAbyB(a, -b);
+powerAB = PoweringAbyB(a, b);
+Console.WriteLine($"A in power B: {powerAB}");
